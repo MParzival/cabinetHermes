67704a511901771f57816622b171c19022f70687
MParzival <mick.aubin@gmail.com>
2019-10-14 17:03:22 +0200

    Feat:Frontend

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -2,18 +2,59 @@
 
 namespace App\Controller;
 
+use App\Entity\Property;
+use App\Repository\PropertyRepository;
+use Doctrine\Common\Persistence\ObjectManager;
+use phpDocumentor\Reflection\Types\Object_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PropertyController extends AbstractController
 {
     /**
-     * @Route("/annonce", name="property_index")
+     * @var PropertyRepository
+     */
+    private $repository;
+    /**
+     * @var ObjectManager
+     */
+    private $em;
+
+    public function __construct(PropertyRepository $repository, ObjectManager $em)
+    {
+        $this->repository = $repository;
+        $this->em = $em;
+    }
+
+    /**
+     * @Route("/annonces", name="property_index")
      */
     public function index()
     {
+        $this->repository->findAllVisible();
         return $this->render('property/index.html.twig', [
             'current_menu'=> 'properties'
         ]);
+    }
+
+    /**
+     * @Route("/annonces/{slug}-{id}", name="property_show", requirements={"slug": "[a-z0-9\-]*"})
+     * @param Property $property
+     * @param string $slug
+     * @return Response
+     */
+    public function show(Property $property, string $slug) : Response
+    {
+        if($property->getSlug() !== $slug){
+            return $this->redirectToRoute('property_show', [
+                'id' => $property->getId(),
+                'slug' => $property->getSlug()
+            ], 301);
+        }
+        return $this->render('property/show.html.twig', [
+            'property' => $property,
+            'current_menu' => 'properties'
+    ]);
     }
 }
--- src/Controller/PublicController.php
+++ src/Controller/PublicController.php
@@ -2,18 +2,23 @@
 
 namespace App\Controller;
 
+use App\Repository\PropertyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class PublicController extends AbstractController
 {
     /**
      * @Route("/", name="app_home")
+     * @param PropertyRepository $repository
+     * @return Response
      */
-    public function index()
+    public function index(PropertyRepository $repository): Response
     {
+        $properties = $repository->findLatest();
         return $this->render('public/home.html.twig', [
-            'controller_name' => 'PublicController',
+            'properties' => $properties
         ]);
     }
 }
