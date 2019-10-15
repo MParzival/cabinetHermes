15abd33ab2bddd5dd17eebcd85232e14fdae39b8
MParzival <mick.aubin@gmail.com>
2019-10-15 15:37:19 +0200

    creation branche develop et update Dev -> parzival

---
+++ src/Controller/PublicController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\PropertyRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class PublicController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_home")
+     * @param PropertyRepository $repository
+     * @return Response
+     */
+    public function index(PropertyRepository $repository): Response
+    {
+        $properties = $repository->findLatest();
+        return $this->render('public/home.html.twig', [
+            'properties' => $properties
+        ]);
+    }
+}
